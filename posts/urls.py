03d20167355fe5d54d5c3ffbd2eddb63be03d780
Kevin Liu <kevin07liu@gmail.com>
2020-02-13 12:26:15 +0800

    [3/27] Render HTML and Markdown

--- posts/urls.py
+++ posts/urls.py
@@ -7,7 +7,7 @@ from . import views
 app_name = "posts"
 urlpatterns = [
     path("", views.post_list, name="list"),
-    path("create/", views.post_create, name="create"),
+    path("create", views.post_create, name="create"),
     path("<slug:slug>", views.post_detail, name="detail"),
     path("<slug:slug>/edit/", views.post_update, name="update"),
     path("<int:id>/delete/", views.post_delete, name="delete"),
